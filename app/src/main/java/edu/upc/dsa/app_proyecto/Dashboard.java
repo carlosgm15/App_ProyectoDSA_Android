a456a77336dfa0f7834decd4f5fef7d809a38925
marchiba99 <marchiba99@gmail.com>
2020-12-18 16:30:56 +0100

    Inicio dashboard

---
+++ app/src/main/java/edu/upc/dsa/app_proyecto/Dashboard.java
@@ -0,0 +1,13 @@
+package edu.upc.dsa.app_proyecto;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Dashboard extends AppCompatActivity {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.dashboard);
+    }
+
+}
